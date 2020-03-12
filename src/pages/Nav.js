85c7c622f6bc5cb43ce9df8731b8fcae41694281
Christoph Tappé <christophsmacbook@chrissie100.home>
2020-03-12 19:01:47 +0100

    Initial commit new branch

---
+++ src/pages/Nav.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Nav() {
+    return (
+      <nav>
+        <FooterStyled>
+          <img src={'./icons/home.svg'} alt="Home"/>
+          <img src={'./icons/settings.svg'} alt="Settings"/>
+          <img src={'./icons/user.svg'} alt="User"/>
+          <img src={'./icons/thumb.svg'} alt="Home"/>
+          <img src={'./icons/foto.svg'} alt="Home"/>
+        </FooterStyled>        
+      </nav>
+    );
+}
+
+const FooterStyled = styled.section` 
+    display: flex;
+    position: fixed;
+    justify-content: space-around;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    padding-left: 16px;
+    padding-right: 16px;
+    background: #c1c1a0;
+    box-shadow: 0 -5px 5px -5px #424235;
+
+    img {
+        display: inline-flex;
+        width: 38px;   
+    }
+`
+
+export default Nav;
