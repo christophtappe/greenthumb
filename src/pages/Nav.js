062096e930fc93d3d95afced9914242e2667d53c
Christoph Tappé <christophsmacbook@chrissie100.home>
2020-03-13 13:53:43 +0100

    Reload Nav-Icons

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -7,18 +7,20 @@ function Nav() {
       <nav>
         <FooterStyled>
           <Link to='/Cards'>
-          <img src={'./icons/home.svg'} alt="Home"/>
+          <img src={'../icons/home.svg'} alt="Home"/>
           </Link>
           <Link to='/Form'>
-          <img src={'./icons/settings.svg'} alt="Settings"/>
+          <img src={'../icons/settings.svg'} alt="Settings"/>
           </Link>
           <Link to='Cards'> 
-          <img src={'./icons/user.svg'} alt="User"/>
+          <img className='imgsize'src={'../icons/icon-chrissie-new.svg'} alt="User"/>
           </Link>
           <Link to='/Greenthumb'>
-          <img src={'./icons/thumb.svg'} alt="Greenthumb"/>
+          <img src={'../icons/thumb.svg'} alt="Greenthumb"/>
           </Link>
-          <img src={'./icons/foto.svg'} alt="Home"/>
+          <Link to='/Fotos'>
+          <img src={'../icons/foto.svg'} alt="Fotos"/>
+          </Link>
         </FooterStyled>        
       </nav>
     );
@@ -27,19 +29,24 @@ function Nav() {
 const FooterStyled = styled.section` 
     display: flex;
     position: fixed;
-    justify-content: space-around;
+    justify-content: space-evenly;
+    align-items: center;
     bottom: 0;
     width: 100%;
     height: 60px;
-    padding-left: 16px;
-    padding-right: 16px;
+    padding-left: 20px;
+    padding-right: 20px;
     background: #c1c1a0;
     box-shadow: 0 -5px 5px -5px #424235;
 
     img {
         display: inline-flex;
-        width: 38px;   
+        align-items: bottom;
+        width: 4px;   
+    }
+
+    .imgsize {
+        width: 50px;
     }
 `
-
 export default Nav;
